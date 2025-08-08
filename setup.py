feb9bae58b86492ceac4271cdab6cb11b9c23c2f
Ephrem0001 <ephrem8118@gmail.com>
2025-08-08 06:59:01 +0300

    chore: initial project structure

---
+++ setup.py
@@ -0,0 +1,7 @@
+﻿from setuptools import setup, find_packages
+setup(
+    name=""Langchain-Documentation-AIChatBot"",
+    version=""0.1.0"",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+)
